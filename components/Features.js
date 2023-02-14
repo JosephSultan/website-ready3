dd90dc3141bf9b5ac80167b98bdda764f8dc81eb
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-14 01:05:56 +0100

    o

--- components/Features.js
+++ components/Features.js
@@ -12,7 +12,7 @@ export default function Features() {
                 Awesome Features
               </div>
               <h1 className="text-3xl font-semibold text-gray-100 md:text-5xl">
-                Decked with countless features.
+              Our most in-demand projects
               </h1>
               <p className="text-xl text-gray-200 md:text-2xl">
                 Each feature is specifcially made for devs and aimed
@@ -38,14 +38,12 @@ export default function Features() {
                 <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
               </svg>
               <h3 className="text-xl font-semibold text-white">
-                Your favorite stack ♡
+              Just getting started?
               </h3>
               <p className="text-gray-200">
-                Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
+                We help you to choose the right support platform for your needs and budget and fully implement it for your team with ease!
+
+
               </p>
             </div>
             <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
@@ -65,14 +63,12 @@ export default function Features() {
                 <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
               </svg>
               <h3 className="text-xl font-semibold text-white">
-                The latest technologies
+              CX Operations systems and metrics building
               </h3>
               <p className="text-gray-200">
-                Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
+              We help you build your internal CX Operational system, KPIs, MOSes and OKRs
+
+
               </p>
             </div>
             <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
@@ -88,21 +84,19 @@ export default function Features() {
                 strokeLinejoin="round"
                 className="border-[#322e37] bg-gradient-to-b from-[#5b585f] to-[#2d2a33] h-10 w-10 rounded-full border p-2 transition duration-400 group-hover:border-[#00e4ff] group-hover:shadow-[#00e4ff] group-hover:shadow-sm"
               >
-                <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
-                <path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
-                <line x1="6" y1="1" x2="6" y2="4"></line>
-                <line x1="10" y1="1" x2="10" y2="4"></line>
-                <line x1="14" y1="1" x2="14" y2="4"></line>
+                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                <circle cx="9" cy="7" r="4"></circle>
+                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
               </svg>
               <h3 className="text-xl font-semibold text-white">
-                One coffee is all it'll take
+              Scaling your processes
+
               </h3>
               <p className="text-gray-200">
-                Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
+              Your contact rate is growing? We got you covered!
+We help scale your internal processes and operations to keep up with the scale at ease
+
               </p>
             </div>
             <div className="flex shadow-md hover:shadow-[#00e4ff] flex-col justify-between p-5 transition duration-400 bg-white bg-opacity-20 rounded-xl card-hover group space-y-5">
