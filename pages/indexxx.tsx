e0cf95395f72706c71e4fa6cd7901322c219b6fc
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-14 01:58:57 +0100

    d

--- pages/indexxx.tsx
+++ pages/indexxx.tsx
@@ -20,19 +20,6 @@ const Home: NextPage = () => {
         <meta property="og:title" content="Example" />
         <meta property="og:image" content="" />
         <link rel="shortcut icon" href="/favicon.png" />
-
-        <script>
-          window.intercomSettings = {
-            api_base: "https://api-iam.intercom.io",
-            app_id: "ltc217cv"
-          };
-        </script>
-
-        <script>
-          // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/ltc217cv'
-          (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ltc217cv';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-        </script>
-
       </Head>
       <div className="h-full leading-normal text-gray-600">
         <div
