633e5dbd5e5fdd3e41cf883ff99b40b8d8f6ab77
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2023-02-14 01:46:26 +0100

    s

--- components/Features.js
+++ components/Features.js
@@ -3,7 +3,7 @@ export default function Features() {
     <section
       id="features"
       className="py-5 bg-gradient-to-t from-[#524790] to-[#8c79ed]"
-      style={{ height: "200px" }}
+      style={{ height: "900px" }}
     >
       <div className="mx-5 max-w-7xl md:mx-10 lg:mx-20 xl:mx-auto">
         <div className="space-y-16 md:space-y-24">
